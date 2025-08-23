6f19b65938d8bf7fdf86fbcb5d53ea3acd403c81
wutwwx <129248868+wutwwx@users.noreply.github.com>
2025-08-23 12:15:38 +0800

    Update cybership2.m

--- model/vessel/vesselModel/cybership2.m
+++ model/vessel/vesselModel/cybership2.m
@@ -1,26 +1,26 @@
-function ship = cybership2()
-%CYBERSHIP2FOSSEN  Returns the parameter structure for the NTNU CyberShip II vessel model.
+function ASV = cybership2()
+%CYBERASV2FOSSEN  Returns the parameter structure for the NTNU CyberShip II vessel model.
 %
-%   ship = cybership2Fossen() returns a structure containing the physical 
+%   ASV = cybership2Fossen() returns a structure containing the physical 
 %   and hydrodynamic parameters for the CyberShip II (NTNU, Fossen) 3-DOF model.
 %   These parameters are commonly used for simulation and control research 
 %   involving small-scale surface vessels.
 %
 %   The structure fields include:
-%     ship.type     : Model type indicator (1 = Fossen 3-DOF)
-%     ship.m, mass  : Vessel mass (kg)
-%     ship.L        : Vessel length (m)
-%     ship.B        : Vessel beam/width (m)
-%     ship.xg       : x-coordinate of center of gravity (m)
-%     ship.Iz       : Yaw moment of inertia (kg·m^2)
-%     ship.m11, m22, m33, m23, m32 : Added mass matrix components
-%     ship.Xu, Xuu, Xuuu           : Hydrodynamic surge damping coefficients
-%     ship.Yv, Yvv, Yr, Yrv, Yvr, Yrr : Sway and yaw hydrodynamic coefficients
-%     ship.Nv, Nvv, Nr, Nrv, Nvr, Nrr : Yaw hydrodynamic coefficients
-%     ship.M     : 3×3 inertia matrix (incl. added mass)
-%     ship.maxtau, mintau : Control input saturation limits
-%     ship.Nx    : State dimension (default 6)
-%     ship.Nu    : Input dimension (default 3)
+%     ASV.type     : Model type indicator (1 = Fossen 3-DOF)
+%     ASV.m, mass  : Vessel mass (kg)
+%     ASV.L        : Vessel length (m)
+%     ASV.B        : Vessel beam/width (m)
+%     ASV.xg       : x-coordinate of center of gravity (m)
+%     ASV.Iz       : Yaw moment of inertia (kg·m^2)
+%     ASV.m11, m22, m33, m23, m32 : Added mass matrix components
+%     ASV.Xu, Xuu, Xuuu           : Hydrodynamic surge damping coefficients
+%     ASV.Yv, Yvv, Yr, Yrv, Yvr, Yrr : Sway and yaw hydrodynamic coefficients
+%     ASV.Nv, Nvv, Nr, Nrv, Nvr, Nrr : Yaw hydrodynamic coefficients
+%     ASV.M     : 3×3 inertia matrix (incl. added mass)
+%     ASV.maxtau, mintau : Control input saturation limits
+%     ASV.Nx    : State dimension (default 6)
+%     ASV.Nu    : Input dimension (default 3)
 %     % Wind parameters (set to 0 by default)
 %
 %   Reference:
@@ -31,8 +31,8 @@ function ship = cybership2()
 %   Date:   2025-06-30
 %
 %   Example usage:
-%     ship = cybership2Fossen;
-%     disp(ship)
+%     ASV = cybership2Fossen;
+%     disp(ASV)
 %
 % -------------------------------------------------------------------------
 %  State vector (for typical use with 3-DOF model):
@@ -46,48 +46,48 @@ function ship = cybership2()
 %
 % -------------------------------------------------------------------------
 
-    ship.type=1;
-    ship.mass=23.8;
-    ship.m=23.8;
-    ship.L=1.255;
-    ship.B = 0.29;
-    ship.xg=0.046;
-    ship.Iz=1.76;
-    ship.Xdot_u=-2.0;
-    ship.Ydot_v=-10.0;        
-    ship.Ydot_r=-0.0;
-    ship.Ndot_v=-0.0;        
-    ship.Ndot_r=-1.0;
-    ship.m11=ship.m-ship.Xdot_u; 
-    ship.m22=ship.m-ship.Ydot_v;  
-    ship.m23=ship.m*ship.xg-ship.Ydot_r; 
-    ship.m32=ship.m*ship.xg-ship.Ndot_v; 
-    ship.m33=ship.Iz-ship.Ndot_r; 
-    ship.Xu=-0.72253; 
-    ship.Xuu=-1.32742; 
-    ship.Xuuu=-5.86643;
-    ship.Yv=-0.88965; 
-    ship.Yvv=-36.47287;
-    ship.Yr=-7.250; 
-    ship.Yrv=-0.805; 
-    ship.Yvr=-0.845; 
-    ship.Yrr=-3.45; 
-    ship.Nv=0.03130; 
-    ship.Nvv=3.95645; 
-    ship.Nr=-1.9; 
-    ship.Nrv=0.13; 
-    ship.Nvr=0.08; 
-    ship.Nrr=-0.75;
-    ship.M=[
-            ship.m11  0    0
-            0   ship.m22  ship.m23
-            0   ship.m32  ship.m33
+    ASV.type=1;
+    ASV.mass=23.8;
+    ASV.m=23.8;
+    ASV.L=1.255;
+    ASV.B = 0.29;
+    ASV.xg=0.046;
+    ASV.Iz=1.76;
+    ASV.Xdot_u=-2.0;
+    ASV.Ydot_v=-10.0;        
+    ASV.Ydot_r=-0.0;
+    ASV.Ndot_v=-0.0;        
+    ASV.Ndot_r=-1.0;
+    ASV.m11=ASV.m-ASV.Xdot_u; 
+    ASV.m22=ASV.m-ASV.Ydot_v;  
+    ASV.m23=ASV.m*ASV.xg-ASV.Ydot_r; 
+    ASV.m32=ASV.m*ASV.xg-ASV.Ndot_v; 
+    ASV.m33=ASV.Iz-ASV.Ndot_r; 
+    ASV.Xu=-0.72253; 
+    ASV.Xuu=-1.32742; 
+    ASV.Xuuu=-5.86643;
+    ASV.Yv=-0.88965; 
+    ASV.Yvv=-36.47287;
+    ASV.Yr=-7.250; 
+    ASV.Yrv=-0.805; 
+    ASV.Yvr=-0.845; 
+    ASV.Yrr=-3.45; 
+    ASV.Nv=0.03130; 
+    ASV.Nvv=3.95645; 
+    ASV.Nr=-1.9; 
+    ASV.Nrv=0.13; 
+    ASV.Nvr=0.08; 
+    ASV.Nrr=-0.75;
+    ASV.M=[
+            ASV.m11  0    0
+            0   ASV.m22  ASV.m23
+            0   ASV.m32  ASV.m33
             ];
-    ship.maxtau=[2,2,1.5];
-    ship.mintau=[-2,-2,-1.5];
-    ship.Nx = 6; 
-    ship.Nu = 3;
-    ship.MaxInputs=[2; 1.5; 1.5];
-    ship.MinInputs=[-2; -1.5; -1.5];
+    ASV.maxtau=[2,2,1.5];
+    ASV.mintau=[-2,-2,-1.5];
+    ASV.Nx = 6; 
+    ASV.Nu = 3;
+    ASV.MaxInputs=[2; 1.5; 1.5];
+    ASV.MinInputs=[-2; -1.5; -1.5];
 
 end
